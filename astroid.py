f380837f72bda609843a602a59bf9be38f617a2e
TorbsT <37838405+TorbsT@users.noreply.github.com>
2020-11-15 19:10:21 +0100

    ctrl+c ctrl+v go brrrr

--- astroid.py
+++
@@ -1,9 +0,0 @@
-import gym
-import Bullet
-import arcade
-env = gym.make("MountainCar-v0")
-env.reset()
-for i in range(1000):
-    env.render()
-    env.step(env.action_space.sample())
-env.close()
